b6f9333eb81791bb2158513bf928b7fb3f6ae9e0
Danil <popovlfybk@yandex.ru>
2026-02-23 22:37:26 +0300

    Update [emotion_analysis]: add option to load local quantized model

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,9 +1,11 @@
-from scraping.scraper import Scraper
+# from scraping.scraper import Scraper
 
-# from emotion_analysis.model import EmotionAnalyzer
+from emotion_analysis.model import EmotionAnalyzer
+
 # from preprocessing.preprocessing_agent import PreprocessingAgent
 
 if __name__ == '__main__':
-    # EmotionAnalyzer().analyze_data()
-    Scraper().start_scraping()
+    EmotionAnalyzer().analyze_data()
+    # PreprocessingAgent().start_preprocessing()
+    # Scraper().start_scraping()
     # PreprocessingAgent().start_preprocessing()
