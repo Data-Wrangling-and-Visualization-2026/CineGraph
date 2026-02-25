b21605a4cd61b8901ee994e5a4cc9107a9fe9a11
Danil <popovlfybk@yandex.ru>
2026-02-25 13:39:26 +0300

    [feat]: add clustering algorithm & database interface layer
    - PostgreSQL with pgvector and ltree were used
    - Hierarchical-styled clustering algorithm with KMeans on data subsets was implemented
    - All the data (including embeddings) are stored in db
    
    Closes Issue #4

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,11 +1,12 @@
-# from scraping.scraper import Scraper
 
-from emotion_analysis.model import EmotionAnalyzer
+# from emotion_analysis.model import EmotionAnalyzer
 
 # from preprocessing.preprocessing_agent import PreprocessingAgent
+from clustering.graph_creator import GraphCreator
 
 if __name__ == '__main__':
-    EmotionAnalyzer().analyze_data()
+    # EmotionAnalyzer().analyze_data()
     # PreprocessingAgent().start_preprocessing()
     # Scraper().start_scraping()
     # PreprocessingAgent().start_preprocessing()
+    GraphCreator().construct_graph()
